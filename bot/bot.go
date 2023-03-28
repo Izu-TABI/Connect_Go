98e9bd5ec55551a9a99f85154fbecd40fe7df456
Izu-TABI <masakisama14@gmail.com>
2023-03-28 12:31:19 +0900

    :Add: bot

---
+++ bot/bot.go
@@ -0,0 +1,52 @@
+package bot
+
+import (
+  "fmt"
+  "Connect2_Go/config"
+
+  "github.com/bwmarrin/discordgo"
+)
+
+var BotId string
+var goBot *discordgo.Session
+
+
+func Start() {
+  goBot, err := discordgo.New("Bot " + config.Token)
+
+  if err != nil {
+    fmt.Println(err.Error())
+    return
+  }
+
+  // Making our bot a user using User function.
+  u, err := goBot.User("@me")
+
+  if err != nil {
+    fmt.Println(err.Error())
+    return
+  }
+
+  // Storing our id from u to BotId
+  BotId = u.ID
+
+
+  goBot.AddHandler(messageHandler)
+
+  err = goBot.Open()
+  if err != nil {
+    fmt.Println(err.Error())
+  }
+
+  fmt.Println("Bot is running!")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+  if m.Author.ID == BotId {
+    return
+  }
+
+  if m.Content == "ping" {
+    _, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+  }
+}
