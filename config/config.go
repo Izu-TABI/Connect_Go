c02ec25b9afb4d1f27944015e17d69d17fabaef7
Izu-TABI <masakisama14@gmail.com>
2023-03-28 12:39:04 +0900

    :Update: config.go

--- config/config.go
+++ config/config.go
@@ -18,7 +18,7 @@ type configStruct struct {
   BotPrefix string `json : "BotPrefix"`
 }
 
-func ReadingConfig() error {
+func ReadConfig() error {
   fmt.Println("Reading config file...")
   file, err := ioutil.ReadFile("./config.json") // ioutil package's ReadFile method which we read config.json and return it's value we will then store it in file variable and if an error ocurrs it will be stored in err .
 
