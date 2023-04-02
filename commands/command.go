b950c23ff74e2f3e3b9d0e315fad975e36aece81
Izu-TABI <masakisama14@gmail.com>
2023-04-02 16:06:13 +0900

    :Add: deploy

--- commands/command.go
+++ commands/command.go
@@ -4,72 +4,67 @@ import (
 	"Connect2_Go/config"
 	"Connect2_Go/voice"
 	"fmt"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-  option string
-  s *discordgo.Session
+	option                   string
+	s                        *discordgo.Session
 	integerOptionMinValue          = 1.0
 	dmPermission                   = false
 	defaultMemberPermissions int64 = discordgo.PermissionManageServer
 
-   Commands = []*discordgo.ApplicationCommand {
-    {
-      Name: "connect",
-      Description: "Connect the voice channel.",
-      Options: []*discordgo.ApplicationCommandOption{
-              {
-                Type:        discordgo.ApplicationCommandOptionChannel,
-                Name:        "channel-option",
-                Description: "Channels",
-                ChannelTypes: []discordgo.ChannelType{
-                  discordgo.ChannelTypeGuildVoice,
-                },
-                Required: true,
-              },
-            },
-    },
-    {
-      Name: "disconnect",
-      Description: "disconnect the voice channel.",
-    },
-
-  }
-	CommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-    "connect": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-      s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-          Content: "Received command to connect.",
-        },
-      })
-      
-      // channel ID of options
-      option = i.ApplicationCommandData().Options[0].Value.(string)
-      voice.Playing = false
-      voice.Play(s, option, "コネクト")
-    },
-
-    "disconnect": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-      vc, err := s.ChannelVoiceJoin(config.GuildId, option, false, true)
-      vc.Disconnect()
-      if err != nil {
-        fmt.Println(err)
-      } 
-
-      s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-          Content: "👋 Bye!",
-        },
-      })
-    },
+	Commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "connect",
+			Description: "Connect the voice channel.",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionChannel,
+					Name:        "channel-option",
+					Description: "Channels",
+					ChannelTypes: []discordgo.ChannelType{
+						discordgo.ChannelTypeGuildVoice,
+					},
+					Required: true,
+				},
+			},
+		},
+		{
+			Name:        "disconnect",
+			Description: "disconnect the voice channel.",
+		},
 	}
-                                    
-)
+	CommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"connect": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Received command to connect.",
+				},
+			})
 
+			// channel ID of options
+			option = i.ApplicationCommandData().Options[0].Value.(string)
+			time.Sleep(time.Duration(300 * time.Millisecond))
+			voice.Play(s, option, "コネクト")
+		},
+
+		"disconnect": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			vc, err := s.ChannelVoiceJoin(config.GuildId, option, false, true)
+			vc.Disconnect()
+			if err != nil {
+				fmt.Println(err)
+			}
   
-
-
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "👋 Bye!",
+				},
+			})
+		},
+	}
+)
